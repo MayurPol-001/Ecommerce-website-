f654db6a5d4d22cca300e96a462aa5f7d9892661
Mayur Pol <polmayur364@gmail.com>
2025-03-20 17:17:47 +0530

    Second commit

--- Electronics/urls.py
+++ Electronics/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from Electronics import views
 
 urlpatterns = [
-    path('Product/', views.Product_details, name='Product_details')
+    path('Product/', views.Product_details, name='Product_details'),
+    path('update/<int:id>', views.update_product, name= 'update_product'),
+    path('delete_product/<int:id>', views.delete_product, name='delete_product'),
 ]
--- Electronics/views.py
+++ Electronics/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Product
 from .forms import ProductForm
 
@@ -16,4 +16,26 @@ def Product_details(request):
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
+            form = ProductForm()
     return render(request,'Electronics/Product_details.html',{'prod':prod,'form':form})
+
+
+
+def update_product(request, id):
+    prod = get_object_or_404(Product, id=id)                    #get the product or return 404
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=prod)         #bind the form to product instance
+        if form.is_valid():
+            form.save()                                          #save the updates product
+            return redirect('Product_details')                    #Redirect to the product list after saving
+    else:         
+        form = ProductForm(instance=prod)                        #populate the form with exsiting product data
+           
+    return render(request,'Electronics/Update_product.html',{'prod':prod,'form':form})
+
+
+def delete_product(request, id):
+    prod = get_object_or_404(Product, id=id)                        #Safe way to get product or return a 404 error
+    prod.delete()            
+    return redirect('Product_details')                               #Redirect back to product details page after deletion
